64038cf9a4e44572474274283274f455009f8208
Jeronimo Romero Tricarico <jerotrica@gmail.com>
2016-11-14 11:02:08 -0300

    mas bar

--- src/ar/edu/unlam/pb2/Bar.java
+++ src/ar/edu/unlam/pb2/Bar.java
@@ -10,5 +10,22 @@ public class Bar {
 	{
 		return this.personas.size();
 	}
+	
+	public void admitirPersona(Persona personas) throws Exception
+	{
+		if(!this.personas.add(personas));
+		{
+			throw new Exception("No se admite");
+		}
+	}
+
+	public TreeSet<Persona> getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(TreeSet<Persona> personas) {
+		this.personas = personas;
+	}
+	
 
 }
