28dae8fa01bbed0d311dc89ca682982df4a2b671
Jeronimo Romero Tricarico <jerotrica@gmail.com>
2016-11-14 10:43:36 -0300

    primer commit

---
+++ src/ar/edu/unlam/pb2/Persona.java
@@ -0,0 +1,55 @@
+package ar.edu.unlam.pb2;
+
+public class Persona {
+	
+	private String nombre;
+	private Integer edad;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
+	public Persona(String nombre)
+	{
+		this.nombre = nombre;
+		this.edad = 0;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
